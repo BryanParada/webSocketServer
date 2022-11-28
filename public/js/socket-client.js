8d4ad6c8cee5a0b860a40d75d7a6a023ba847ff9
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-28 17:42:39 -0300

    N205 - Connection Messages - Client

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,20 @@
+//Referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
 
-//socket del cliente
+//socket del cliente / Navegador!
 const socket = io();
+
+socket.on('connect', () =>{ 
+    console.log('Connected'); 
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () =>{ 
+    console.log('Disconnected from Server');
+    
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
